08ddcc4c7aad524c208a67fa064224f2958ba13f
Haqq Tertuliano <35573633+Htertuliano@users.noreply.github.com>
2018-04-12 17:27:37 -0400

    Add files via upload

---
+++ js/pokemon.js
@@ -0,0 +1,29 @@
+class pokemon  {
+
+		pokemon(String name, String type, double hp, double def, double atk, boolean legend) {
+
+		this.name = name;
+		this.type = type;
+		this.hp = hp;
+		this.def = def;
+		this.atk = atk;
+		this.legend = legend;
+		}
+
+
+		attack(pokemon) {
+		
+		}
+		
+
+
+
+
+
+
+
+
+
+}
+
+
